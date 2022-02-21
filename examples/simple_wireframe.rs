5add49aa9079b8368b8c3f7eabec0fcddfb565c2
Aceeri <conmcclusk@gmail.com>
2022-02-21 01:50:25 -0800

    Initial publish for wireframe stuff

--- examples/simple_wireframe.rs
+++ examples/simple_wireframe.rs
@@ -54,11 +54,11 @@ pub struct Accumulate(f32);
 pub fn rotate_camera(
     mut accumulate: ResMut<Accumulate>,
     time: Res<Time>,
-    mut cameras: Query<(&mut Transform, &Camera)>,
+    mut cameras: Query<&mut Transform, With<Camera>>,
 ) {
     accumulate.0 += time.delta_seconds() / 5.0;
     let scale = 3.0;
-    for (mut transform, cam) in cameras.iter_mut() {
+    for mut transform in cameras.iter_mut() {
         *transform =
             Transform::from_xyz(accumulate.0.cos() * scale, (accumulate.0 / 1.5).cos() * scale, accumulate.0.sin() * scale)
                 .looking_at(Vec3::ZERO, Vec3::Y);
